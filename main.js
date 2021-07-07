f94e4d37b52130b1fcf7a6af253b333ed902ed41
Whitney H <whitneyhuong@gmail.com>
2021-07-07 15:59:24 -0400

    added gsap animations to index

--- main.js
+++ main.js
@@ -69,65 +69,294 @@ gsap.registerPlugin(ScrollTrigger);
 
 // Hero Title Animation
 
-gsap.from("#hero-title", {
-	duration: 3.2,
-	x: "-150",
-	ease: "ease",
-});
+function heroAnim() {
+	const timeline = gsap.timeline();
 
-gsap.from(".hero img", {
-	duration: 3.2,
-	scale: "1.5",
-	ease: "ease",
-});
+	timeline.from(
+		".hero img",
+		{
+			scale: 1.5,
+			duration: 1.5,
+			ease: "ease",
+		},
+		1
+	);
+}
+
+heroAnim();
+
+// Intro
+
+function introSectionAnim() {
+	gsap.from(".intro-section .img-container .large-img", {
+		scrollTrigger: {
+			trigger: ".intro-section",
+			start: "top center",
+			end: "bottom center",
+			toggleActions: "play none none reverse",
+		},
+		ease: "ease",
+		x: -300,
+		opacity: 0,
+		duration: 1,
+	});
+
+	gsap.from(".small-img", {
+		scrollTrigger: {
+			trigger: ".intro-section",
+			start: "top center",
+			end: "bottom center",
+			toggleActions: "play none none reverse",
+		},
+		ease: "ease",
+		y: 300,
+		opacity: 0,
+		duration: 1.5,
+	});
+
+	ScrollTrigger.saveStyles(".intro-txt");
+
+	ScrollTrigger.matchMedia({
+		"(min-width: 1920px)": function () {
+			gsap.from(".intro-txt", {
+				scrollTrigger: {
+					trigger: ".intro-section",
+					start: "top center",
+					end: "bottom bottom",
+					toggleActions: "play none none reverse",
+				},
+				x: 100,
+				opacity: 0,
+				duration: 1.5,
+				ease: "ease",
+			});
+		},
+		"(max-width: 1919px)": function () {
+			gsap.from(".intro-txt", {
+				scrollTrigger: {
+					trigger: ".intro-txt",
+					start: "top center+=400",
+					end: "bottom center",
+					toggleActions: "play none none reverse",
+				},
+				y: 100,
+				opacity: 0,
+				duration: 1.5,
+				ease: "ease",
+			});
+		},
+	});
+}
+
+introSectionAnim();
 
 // Parallax Animation
 
-gsap.from(".bg-parallax img", {
-	scrollTrigger: {
-		trigger: ".bg-parallax",
-		start: "top center",
-		toggleActions: "restart restart restart restart",
-	},
-	ease: Power1.ease,
-	scale: 2,
-	duration: 2,
-});
+function parallaxAnim() {
+	gsap.from(".bg-parallax img", {
+		scrollTrigger: {
+			trigger: ".bg-parallax",
+			start: "top center",
+			end: "bottom center",
+			toggleActions: "restart none restart none",
+		},
+		ease: Power1.ease,
+		scale: 2,
+		duration: 2,
+	});
 
-gsap.from(".bg-parallax2 img", {
-	scrollTrigger: {
-		trigger: ".bg-parallax2",
-		toggleActions: "restart restart restart restart",
-	},
-	ease: Power1.ease,
-	scale: 2,
-	duration: 2,
-});
+	gsap.from(".bg-parallax2 img", {
+		scrollTrigger: {
+			trigger: ".bg-parallax2",
+			start: "top center",
+			end: "bottom center",
+			toggleActions: "restart none restart none",
+		},
+		ease: Power1.ease,
+		scale: 2,
+		duration: 2,
+	});
+}
+
+parallaxAnim();
+
+//menu section
+
+function menuSectionAnim() {
+	const menuTl = gsap.timeline({
+		scrollTrigger: {
+			trigger: ".menu-section",
+			start: "top center",
+			ease: "ease",
+			toggleActions: "play none none reverse",
+		},
+	});
+	menuTl
+		.from(".menu-section .img-container .menu-stag", {
+			opacity: 0,
+			duration: 1,
+			stagger: 0.5,
+			ease: "ease",
+		})
+		.from(".menu-section .span-titles", {
+			y: 300,
+			opacity: 0,
+			ease: "ease",
+		})
+		.from(
+			".menu-section .span-titles span",
+			{
+				y: 100,
+				opacity: 0,
+				stagger: 0.2,
+				ease: "ease",
+			},
+			"label"
+		);
+}
+
+menuSectionAnim();
 
 // Gallery Page
 
-gsap.from(".gallery-bg h2", {
-	duration: 2,
-	scale: "0.5",
-	ease: "ease",
-});
+function galleryAnim() {
+	gsap.from(".gallery-bg h2", {
+		duration: 1,
+		scale: "0.5",
+		delay: 0.5,
+		ease: "ease",
+	});
 
-gsap.from(".gallery-bg img", {
-	duration: 2,
-	scale: "1.5",
-	ease: "ease",
-});
+	gsap.from(".gallery-bg img", {
+		duration: 1,
+		delay: 0.5,
+		scale: "1.5",
+		ease: "ease",
+	});
+}
+
+galleryAnim();
 
 // Service Page
 
-gsap.from(".service-bg h2", {
-	duration: 1.5,
-	scale: "0.5",
-	ease: "ease",
-});
+function serviceAnim() {
+	gsap.from(".service-bg h2", {
+		duration: 1,
+		scale: "0.5",
+		delay: 0.5,
+		ease: "ease",
+	});
 
-gsap.from(".service-bg img", {
-	duration: 1.6,
-	scale: "1.5",
-	ease: "ease",
-});
+	gsap.from(".service-bg img", {
+		duration: 1,
+		delay: 0.5,
+		scale: "1.5",
+		ease: "ease",
+	});
+}
+
+serviceAnim();
+// Contact
+
+function contactAnim() {
+	const contactTl = gsap.timeline({ delay: 0.1 });
+
+	contactTl
+		.set(".img-container .redgirl", {
+			zIndex: 2,
+		})
+		.from(
+			".img-container #sunflowers",
+			{
+				yPercent: 50,
+				opacity: 0,
+				duration: 1.2,
+			},
+			"contact"
+		)
+		.from(
+			".img-container .redgirl",
+			{
+				xPercent: 50,
+				opacity: 0,
+				duration: 1.2,
+			},
+			"contact"
+		)
+		.from(".contact-container img", {
+			xPercent: 50,
+			opacity: 0,
+			duration: 1.2,
+		})
+		.from(
+			".contact-container h2",
+			{
+				xPercent: 50,
+				opacity: 0,
+				duration: 1.2,
+			},
+			"contact"
+		)
+		.from(
+			".contact-container p",
+			{
+				opacity: 0,
+				duration: 1,
+			},
+			0.9
+		);
+
+	gsap.from(".stag", {
+		scrollTrigger: {
+			trigger: ".contact-info",
+			start: "top center+=300",
+			end: "bottom center",
+			toggleActions: "play none none none",
+		},
+		ease: "ease",
+		y: 100,
+		opacity: 0,
+		duration: 0.8,
+		stagger: 0.2,
+	});
+}
+
+contactAnim();
+
+// About
+function aboutAnim() {
+	const aboutTl = gsap.timeline();
+
+	aboutTl
+		.set(".about .img-container img", {
+			zIndex: -1,
+		})
+		.from(".about .img-container img", {
+			y: 300,
+			duration: 1,
+			opacity: 0,
+		})
+		.from(
+			"#frame",
+			{
+				duration: 1,
+				opacity: 0,
+			},
+			0.2
+		)
+		.from(
+			".about-container h2",
+			{
+				opacity: 0,
+				x: 300,
+				duration: 1.2,
+			},
+			0.2
+		)
+		.from(".about-container p", {
+			opacity: 0,
+			duration: 1,
+		});
+}
+
+aboutAnim();
